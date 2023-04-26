6d896c65765e5a9f2d2403648a6e50680f83b397
Tyler Tomani <ttomani@gmail.com>
2023-04-26 04:01:51 -0400

    Mobile Trial

---
+++ js/index.js
@@ -0,0 +1,97 @@
+const chapters = document.querySelectorAll('.chapter')
+const lessons = document.querySelectorAll('.chapter > ul li a')
+const aside = document.querySelector('.chapter-container')
+const iframe = document.querySelector('iframe')
+const chapterDisplay = document.querySelector('.chapter-display')
+const lessonDisplay = document.querySelector('.lesson-display')
+let lessonArray = []
+let currentLesson = ''
+
+// Event listeners for each chapter drop menus
+hideLessons()
+chapters.forEach(chapter => {
+    const chapterLink = chapter.querySelector('.chapter-link')
+    const lessonsUl = chapter.querySelector('ul')
+    chapterLink.addEventListener('click', e => {
+        e.preventDefault()
+        e.stopPropagation()
+        hideLessons()
+        toggleLessons(lessonsUl)
+    })
+})
+
+function hideLessons(){
+    chapters.forEach(chapter => {
+        let ul = chapter.querySelector('ul')
+        if(!ul.classList.contains('hide') && !ul.classList.contains('show')){
+            ul.classList.add('hide')
+        }     
+    })
+}
+function toggleLessons(ul){
+    let parent = ul.parentElement
+    let titleChapter = parent.querySelector('a')
+    lessonDisplay.innerHTML = ''
+    chapterDisplay.innerText = titleChapter.innerText
+    if(ul.classList.contains('hide')){
+        ul.classList.remove('hide')
+    } else if(ul.classList.contains('show')) {
+        ul.classList.remove('show')
+        ul.classList.add('hide')
+    } else {
+        ul.classList.add('hide')
+    }
+
+}
+
+
+// Even listeners for loading each lesson with mouse click and enter key
+lessons.forEach(lesson => {
+/////// Delete this when not working on the site
+    if(lesson.hasAttribute('autofocus')){
+        iframe.src = lesson.href
+    }
+///////////////////////////////////
+
+// key press if enter is hit twice on lesson, iframe's lesson gets focus
+    lesson.addEventListener('keydown', e => {
+        let key = e.keyCode
+        if(key === 13){
+            e.preventDefault()
+            e.stopPropagation()
+            const chapterCurrent =  e.target.parentElement.parentElement.parentElement
+            const titleChapter = chapterCurrent.querySelector('a')
+            console.log(titleChapter.innerText)
+            chapterDisplay.innerText = titleChapter.innerText
+            lessonDisplay.innerHTML = "&rarr; " + e.target.innerText
+            currentLesson = e.target
+
+            iframe.src = currentLesson.href
+            lessonArray.unshift(currentLesson)
+            if(lessonArray.length > 2){
+                lessonArray.pop()
+            }
+            if(lessonArray[0] == lessonArray[1]){
+                iframe.focus()
+            }
+        }        
+    })
+// If lesson is clicked with mouse once, iframe's lesson receives focus
+    lesson.addEventListener('click', e => {
+        e.preventDefault()
+        e.stopPropagation()
+        const chapterCurrent =  e.target.parentElement.parentElement.parentElement
+        const titleChapter = chapterCurrent.querySelector('a')
+        console.log(titleChapter.innerText)
+        chapterDisplay.innerText = titleChapter.innerText
+        lessonDisplay.innerHTML = "&rarr; " + e.target.innerText
+        currentLesson = e.target
+        iframe.src = currentLesson.href
+    })
+    iframe.addEventListener('focusout', e => {
+        currentLesson.focus()           
+    })
+})
+
+
+
---
+++ js/videos.js
@@ -0,0 +1,78 @@
+const videoTxtContainers = document.querySelectorAll('.video-txt-container')
+const videos = document.querySelectorAll('.video-container > video')
+let videoPlaying = false
+
+videos.forEach(vid => {
+    vid.addEventListener('click', e => {
+        let video = e.target
+        if(!videoPlaying){
+            video.play()
+        } else { 
+            video.pause()
+        }
+        videoPlaying = !videoPlaying
+        video.addEventListener('ended', () => {
+            video.currentTime = 0
+        })
+        console.log("video clicked: ",videoPlaying)
+    })
+})
+
+
+videoTxtContainers.forEach(videoTxtContainer => {
+    let videoTxt = videoTxtContainer.querySelector('.txt')
+    /* For some reason when clicking in certain places within the video-txt-container the 
+       the parent becomes the body or the HTML page, So i have to target the txt divs and the 
+       p's within the txt divs, Very Very Frustrating */
+    let videoPs = videoTxt.querySelectorAll('p')
+    videoPs.forEach(p => {
+        p.addEventListener('click', event => {
+            playPauseVideo(event)
+        })
+    })
+
+    videoTxt.addEventListener('click', event => {
+        playPauseVideo(event)
+
+    })
+    videoTxtContainer.addEventListener('click', event => {
+        playPauseVideo(event)
+    })
+    videoTxtContainer.addEventListener('keydown', event => {
+        if(event.keyCode === 13){
+            playPauseVideo(event)
+        }
+    })
+})
+
+function playPauseVideo(e){
+    let parent = e.target.parentElement
+// // // // // // // // // // // // // // // // // // // // // // // // // 
+    /** There has to be a more efficient way than this */
+    if(parent.classList.contains('fc')){
+        // parent = parent
+    } else if(parent.classList.contains('video-txt-container')){
+        // parent = e.target.parentElement.parentElement
+    } else if(parent.classList.contains('txt')){
+        parent = e.target.parentElement.parentElement.parentElement
+    } else if(parent.classList.contains('shortcut-txt')){
+        parent = e.target.parentElement.parentElement.parentElement.parentElement
+    } else if(parent.classList.contains('txt-p')){
+        parent = e.target.parentElement.parentElement.parentElement.parentElement
+    }
+// // // // // // // // // // // // // // // // // // // // // // // // // 
+    console.log('parent',parent)
+    let video = parent.querySelector('.video-container > video')
+    if(!videoPlaying){
+        video.play()
+    } else { 
+        video.pause()
+    }
+    videoPlaying = !videoPlaying
+   
+    video.addEventListener('ended', () => {
+        video.currentTime = 0
+
+    })
+    // console.log("text clicked: ",videoPlaying)
+}
